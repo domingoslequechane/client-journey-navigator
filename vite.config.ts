99c320a9cb65a1e9ab6e11a8dae9388551b5a2c7
Domingos Francisco Lequechane <domingosf.lequechane@gmail.com>
2026-04-29 14:53:04 +0200

    fix: increase PWA cache limit for large bundle

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         runtimeCaching: [
           {
